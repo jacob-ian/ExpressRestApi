ef435617182a3fdf7cf5b85a081c71326913e4be
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-02-19 20:52:04 +0800

    fix(Server): Various fixes to Server Class.
    - Add server activity boolean
    - Add callback method handling to start() and stop()
    - Rename class properties (express and server)

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,8 +4,11 @@ import bodyParser from 'body-parser';
 
 export class Server {
   private EXTERNAL_PORT: number;
-  private expressApp = express();
-  private expressServer: any;
+
+  private express = express();
+  private server: any;
+
+  private isServerActive: boolean = false;
 
   constructor(port: number) {
     this.EXTERNAL_PORT = port;
@@ -18,32 +21,73 @@ export class Server {
   }
 
   private connectThirdPartyMiddleware(): void {
-    this.expressApp.use(cors());
-    this.expressApp.use(bodyParser.urlencoded({ extended: true }));
+    this.express.use(cors());
+    this.express.use(bodyParser.urlencoded({ extended: true }));
   }
 
   private connectApiRoutes(): void {
-    this.expressApp.get('/', (req, res) => {
+    this.express.get('/', (req, res) => {
       return res.status(200).send();
     });
   }
 
-  public start(): void {
-    this.expressServer = this.expressApp.listen(this.EXTERNAL_PORT, () => {
-      this.logServerStarted();
+  public start(callback?: () => void): void {
+    if (this.serverIsInactive()) {
+      this.startServerListening(callback);
+    }
+  }
+
+  private serverIsInactive(): boolean {
+    return !this.isServerActive;
+  }
+
+  private startServerListening(callback?: () => void): void {
+    this.server = this.express.listen(this.EXTERNAL_PORT, () => {
+      this.isServerActive = true;
+      this.notifyServerStart();
+      if (callback) {
+        callback();
+      }
     });
   }
 
-  private logServerStarted(): void {
-    console.log(`🌐 Server is listening on port: ${this.EXTERNAL_PORT}`);
+  private notifyServerStart(): void {
+    this.logServerStarted();
   }
 
-  public stop(): void {
-    this.expressServer.close();
+  private logServerStarted(): void {
+    console.log(`🌐 Server is listening on port: ${this.EXTERNAL_PORT}.`);
+  }
+
+  public stop(callback?: () => void): void {
+    if (this.serverIsActive()) {
+      this.stopServerListening(callback);
+    }
+  }
+
+  private serverIsActive(): boolean {
+    return this.isServerActive;
+  }
+
+  private stopServerListening(callback?: () => void): void {
+    return this.server.close(() => {
+      this.isServerActive = false;
+      this.notifyServerStopped();
+      if (callback) {
+        callback();
+      }
+    });
+  }
+
+  private notifyServerStopped(): void {
     this.logServerStopped();
   }
 
   private logServerStopped(): void {
     console.log('🛑 Server has been stopped.');
+  }
+
+  public getExpressServer(): any {
+    return this.server;
   }
 }
