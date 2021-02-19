d357749c07836e63c42bd6ddf467a7a68c10228f
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-02-19 20:53:01 +0800

    test(Server): Create Start-Up and Shut-Down tests

--- src/server/server.spec.ts
+++ src/server/server.spec.ts
@@ -1,4 +1,33 @@
 import assert from 'assert';
+import { LogCapturer } from '../utils/log-capturer';
 import { Server } from './server';
 
-describe('Express Server', function () {});
+const SERVER_PORT = 8000;
+describe('Express Server', function () {
+  describe('Server Start Up and Shut Down', function () {
+    describe('Start and Stop Messages', function () {
+      let server = new Server(SERVER_PORT);
+      let consoleCapturer = new LogCapturer();
+
+      before(function (done) {
+        consoleCapturer.startCapture();
+        server.start(() => {
+          server.stop(() => {
+            consoleCapturer.stopCapture();
+            done();
+          });
+        });
+      });
+
+      it('Should log a "Listening" and "Stop" Message on Cycle.', function () {
+        const listeningMessage = `🌐 Server is listening on port: ${SERVER_PORT}.`;
+        const stoppedMessage = '🛑 Server has been stopped.';
+
+        const expectedLogs = [listeningMessage, stoppedMessage];
+        const capturedLogs = consoleCapturer.getLoggedText();
+
+        assert.deepStrictEqual(capturedLogs, expectedLogs);
+      });
+    });
+  });
+});
