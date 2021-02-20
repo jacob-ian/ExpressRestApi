8256be67490c8c309a3c2224a32617c757f08b7a
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-02-20 10:47:35 +0800

    feat(Controllers): Create generic Controller Class
    - Create Controller Class to simplify CRUD operations

---
+++ src/server/controllers/controller.ts
@@ -0,0 +1,11 @@
+export class Controller {
+  public getList(req: Express.Request, res: Express.Response): void {}
+
+  public getById(req: Express.Request, res: Express.Response): void {}
+
+  public create(req: Express.Request, res: Express.Response): void {}
+
+  public updateById(req: Express.Request, res: Express.Response): void {}
+
+  public deleteById(req: Express.Request, res: Express.Response): void {}
+}
