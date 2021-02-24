e7c42a66d35ed390cf21d38d252739a880121fb5
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-02-24 16:19:15 +0800

    fix(Exception): Add context property to Exceptions
    - Add context to attach error objects and create an exception chain.

--- src/utils/exception.ts
+++ src/utils/exception.ts
@@ -2,13 +2,16 @@ export class Exception {
   private errorId: string;
   private error: string;
   private errorMessage: string | undefined;
+  private errorContext: any;
 
   constructor(errorId: string, error: string);
   constructor(errorId: string, error: string, message: string);
-  constructor(errorId: string, error: string, message?: string) {
+  constructor(errorId: string, error: string, message: string, context: any);
+  constructor(errorId: string, error: string, message?: string, context?: any) {
     this.errorId = errorId;
     this.error = error;
     this.errorMessage = message;
+    this.errorContext = context;
   }
 
   public getMessage(): string {
@@ -17,5 +20,13 @@ export class Exception {
 
   private formatMessage(): string {
     return this.errorMessage ? `: ${this.errorMessage}` : '';
+  }
+
+  public getErrorCode(): string {
+    return this.error;
+  }
+
+  public getContext(): any {
+    return this.errorContext;
   }
 }
--- src/utils/file-system-exception.ts
+++ src/utils/file-system-exception.ts
@@ -1,11 +1,12 @@
 import { Exception } from './exception';
 
 export class FileSystemException extends Exception {
-  constructor(error: string);
-  constructor(error: string, message: string);
-  constructor(error: string, message?: string) {
+  constructor(error: 'not-found' | 'unknown');
+  constructor(error: 'not-found' | 'unknown', message: string);
+  constructor(error: 'not-found' | 'unknown', message: string, context: any);
+  constructor(error: 'not-found' | 'unknown', message?: string, context?: any) {
     if (message) {
-      super('FSx', error, message);
+      super('FSx', error, message, context);
     } else {
       super('FSx', error);
     }
